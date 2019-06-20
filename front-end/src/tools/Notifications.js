8361a606c0b02fd8b7e9a2c4207f9787c754234c
bryancode13 <lbryan.code@gmail.com>
2019-06-19 23:39:57 -0500

    Remove Front-End Folder

--- front-end/src/tools/Notifications.js
+++
@@ -1,8 +0,0 @@
-import { message } from 'antd';
-
-export default class Notifications {
-    static openNotificationWithIcon = (type, messageText, description) => {
-        message[type]( messageText + " " + description);
-    };
-
-}
